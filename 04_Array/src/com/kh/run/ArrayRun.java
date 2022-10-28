85f03a68da29b76e85ce5f609baad22f6664bc05
KIMSUJIN98 <qwer9402@naver.com>
2022-10-28 15:09:02 +0900

    Array_kh
    Array_kh

--- 04_Array/src/com/kh/run/ArrayRun.java
+++ 04_Array/src/com/kh/run/ArrayRun.java
@@ -10,7 +10,16 @@ public class ArrayRun {
 		//a.method1();
 		//a.method2();
 		//a.method3();
-		a.method4();
+		//a.method4();
+		//a.method5();
+		//a.method6();
+		//a.method7();
+		//a.method8();
+		//a.method9();
+		//a.method10();
+		a.method11();
+		
+		//a.method99();
 	}
 
 }
